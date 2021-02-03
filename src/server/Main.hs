452835f3a7bf7d7cc6dbf430ff067b1a38224b37
samipourquoi <sami.guiderdoni@gmail.com>
2021-02-03 23:36:24 +0100

    Add static content delivery

--- src/server/Main.hs
+++ src/server/Main.hs
@@ -1,8 +1,10 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 import Web.Scotty
+import Network.Wai.Middleware.Static
 
 main :: IO ()
-main = scotty 3000 $
-    get "/" $ do
-        html $ mconcat ["<h1>Scotty, ", "test", " me up!</h1>"]
+main = scotty 3000 $ do
+  -- static content
+  get "/" $ file "dist/index.html"
+  middleware $ staticPolicy $ addBase "dist"
