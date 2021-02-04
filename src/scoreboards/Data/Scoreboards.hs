c8a266f9876a7810cf15c04828241d4c81eb33a5
samipourquoi <sami.guiderdoni@gmail.com>
2021-02-04 12:42:14 +0100

    Generate scoreboard database

--- src/scoreboards/Data/Scoreboards.hs
+++ src/scoreboards/Data/Scoreboards.hs
@@ -1,4 +1,27 @@
-module Data.Scoreboards (readData) where
+module Data.Scoreboards
+(   Scoreboard,
+    readFromCSV,
+    generateDatabase ) where
 
-readData :: String
-readData = "hello world!"
+import qualified Data.Map as Map
+import           Text.CSV (parseCSVFromFile)
+
+data Scoreboard = Scoreboard String Integer String
+    deriving (Show)
+
+readFromCSV :: FilePath -> IO [Scoreboard]
+readFromCSV path = do
+    content <- parseCSVFromFile path
+    return $ case content of
+        Left err -> []
+        Right content -> map readRow content
+    where
+        readRow :: [String] -> Scoreboard
+        readRow (name : value : objective : _) = Scoreboard name (read value) objective
+
+generateDatabase :: [String] -> IO [Scoreboard]
+generateDatabase s = do
+    nestedScoreboards <- mapM (readFromCSV . toFileName) s
+    return $ concat nestedScoreboards
+    where
+        toFileName s = "data/" ++ s ++ ".csv"
--- src/server/Main.hs
+++ src/server/Main.hs
@@ -2,19 +2,20 @@
 
 import Web.Scotty
 import Network.Wai.Middleware.Static
-import Data.Scoreboards
+import Data.Scoreboards (generateDatabase)
+import Control.Monad
 
 main :: IO ()
 main = do
-  putStrLn readData
+    db <- generateDatabase ["endtech"]
 
-  scotty 3000 $ do
-    -- static content
-    get "/" $ file "dist/index.html"
-    middleware $ staticPolicy $ addBase "dist"
+    scotty 3000 $ do
+        -- static content
+        get "/" $ file "dist/index.html"
+        middleware $ staticPolicy $ addBase "dist"
 
-    -- api
-    get "/api/:server/:objective" $ do
-      server <- param "server"
-      objective <- param "objective"
-      text $ server <> " " <> objective
+        -- api
+        get "/api/:server/:objective" $ do
+        server <- param "server"
+        objective <- param "objective"
+        text $ server <> " " <> objective
